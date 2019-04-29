82a238f135eb03d385f46610c4b9161682c5e596
gcth <gcth@dsnet.pl>
2019-04-29 21:42:35 +0200

    deprecated for frameworkextra - broken also

--- Controller/ApiController.php
+++ Controller/ApiController.php
@@ -28,7 +28,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
- * @Route("/api", service="jms_translation.controller.api_controller")
+ * @Route("/api")
  *
  * @author Johannes M. Schmitt <schmittjoh@gmail.com>
  */
--- Controller/TranslateController.php
+++ Controller/TranslateController.php
@@ -29,7 +29,7 @@ use Symfony\Component\HttpFoundation\Request;
 /**
  * Translate Controller.
  *
- * @Route(service="jms_translation.controller.translate_controller")
+ * @Route()
  * @author Johannes M. Schmitt <schmittjoh@gmail.com>
  */
 class TranslateController
